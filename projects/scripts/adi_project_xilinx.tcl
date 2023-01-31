37549403b54b3267ba7a5b259d6bf50fe7005f6f
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2023-01-31 12:20:47 +0000

    Revert: tcl: Change Vivado version to 2020.1

--- projects/scripts/adi_project_xilinx.tcl
+++ projects/scripts/adi_project_xilinx.tcl
@@ -458,10 +458,10 @@ proc adi_project_run {project_name} {
   set timing_string $[report_timing_summary -return_string]
   if { [string match "*VIOLATED*" $timing_string] == 1 ||
        [string match "*Timing constraints are not met*" $timing_string] == 1} {
-    write_hw_platform -fixed -force  -include_bit -file ${actual_project_name}.sdk/system_top_bad_timing.xsa
+    file copy -force $project_name.runs/impl_1/system_top.sysdef $project_name.sdk/system_top_bad_timing.hdf
     return -code error [format "ERROR: Timing Constraints NOT met!"]
   } else {
-    write_hw_platform -fixed -force  -include_bit -file ${actual_project_name}.sdk/system_top.xsa
+    file copy -force $project_name.runs/impl_1/system_top.sysdef $project_name.sdk/system_top.hdf
   }
 }
 
